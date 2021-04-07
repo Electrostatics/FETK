6868aa4a9a9a8bf4ec954a46b61ec878bdf57a4c
Noah Oblath <nsoblath@gmail.com>
2021-04-06 17:27:37 -0700

    First commit of punc's CMakeLists.txt build.  Some basic config functionality and starting to generalize some FETK CMake functionality

---
+++ cmake/FETKBuildFunctions.cmake
@@ -0,0 +1,63 @@
+# FETKBuildFunctions.cmake
+#
+# Author: N.S. Oblath
+# Created: Apr 6, 2021
+
+
+set(BUILD_SHARED_LIBRARIES OFF)
+
+
+################################################################################
+# set_basic_vars_and_paths (macro)
+#
+# Use this macro just after the project() command to set common variables and paths
+#
+# No arguments
+################################################################################
+macro(set_basic_vars_and_paths)
+
+    ################################################################################
+    # Set package variables
+    ################################################################################
+    
+
+    set(PUNC_VERSION ${maloc_VERSION})
+    set(FETK_VERSION ${FETK_VERSION})
+    set(PACKAGE_NAME ${PROJECT_NAME})
+    set(PACKAGE_TARNAME ${PROJECT_NAME})
+    set(PACKAGE_VERSION "${punc_VERSION_MAJOR}.${punc_VERSION_MINOR}")
+    set(PACKAGE_STRING "${PROJECT_NAME} ${PACKAGE_VERSION}")
+    set(PACKAGE_BUGREPORT "mholst@math.ucsd.edu")
+
+
+    ################################################################################
+    # Set project paths
+    ################################################################################
+
+    message(STATUS "Setting project paths")
+
+    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+        set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}" CACHE PATH "Install prefix" FORCE)
+    endif()
+    message(STATUS "Install prefix: ${CMAKE_INSTALL_PREFIX}")
+
+    set(CMAKE_INSTALL_INCLUDEDIR include/${PROJECT_NAME})
+    include(GNUInstallDirs)
+endmacro()
+
+
+################################################################################
+# find_or_build (function)
+#
+# Finds a library or builds it if it isn't found
+################################################################################
+function(find_or_build)
+
+    set(options OPTIONAL FAST)
+    set(oneValueArgs DESTINATION RENAME)
+    set(multiValueArgs TARGETS CONFIGURATIONS)
+
+    cmake_parse_arguments(PARSE_ARGV 0 FOB_ options oneValueArgs multiValueArgs )
+
+
+endfunction()
