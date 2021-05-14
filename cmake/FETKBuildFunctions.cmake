8e980b1a26b4440c5961900cc9363b8372c019ab
Noah Oblath <nsoblath@gmail.com>
2021-05-14 14:16:12 -0700

    Saving some uncommitted changes

--- cmake/FETKBuildFunctions.cmake
+++ cmake/FETKBuildFunctions.cmake
@@ -85,7 +85,69 @@ function(find_or_build)
     set(oneValueArgs DESTINATION RENAME)
     set(multiValueArgs TARGETS CONFIGURATIONS)
 
-    cmake_parse_arguments(PARSE_ARGV 0 FOB_ options oneValueArgs multiValueArgs )
+    cmake_parse_arguments(PARSE_ARGV 0 FOB options oneValueArgs multiValueArgs )
 
 
 endfunction()
+
+
+################################################################################
+# setup_for_build (macro)
+#
+# Prepares the variables that will be used to build the library
+################################################################################
+macro(setup_for_build)
+    set(LIB_BUILD_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+    set(ALLOWABLE_TYPES "SOURCES;EXTERNAL_HEADERS;INTERNAL_HEADERS")
+    set(SOURCES)
+    set(EXTERNAL_HEADERS)
+    set(INTERNAL_HEADERS)
+endmacro()
+
+
+################################################################################
+# add_build_items (macro)
+#
+# Adds items (sources, internal headers, and external headers) for the build
+#
+# Arguments:
+#   TYPE: options are
+#           - SOURCES
+#           - INTERNAL_HEADERS
+#           = EXTERNAL HEADERS
+#   ITEMS: space-separated list of files (relative or absolute paths)
+################################################################################
+macro(add_build_items TYPE)
+
+    set(oneValueArgs TYPE)
+    set(multiValueArgs ITEMS)
+    #cmake_parse_arguments(ABI "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(ABI "" "TYPE" "ITEMS" ${ARGN})
+
+    message(STATUS "Type: ${ABI_TYPE}")
+    message(STATUS "Items: ${ABI_ITEMS}")
+
+    # Make sure the TYPE argument is valid
+    if(NOT "${ABI_TYPE}" IN_LIST ALLOWABLE_TYPES)
+        message(
+            FATAL_ERROR
+            "Passed an invalid type to add_build_items macro.  "
+            "Must be one of ${ALLOWABLE_TYPES}.  "
+            "Was ${ABI_TYPE}."
+        )
+    endif()
+    
+    # Add each item successively
+    foreach(ITEM ${ABI_ITEMS})
+        get_filename_component(ITEM_PATH ${ITEM} ABSOLUTE)
+        
+        # Add the item to the appropriately typed list
+        list(APPEND ${ABS_TYPE} "${ITEM_PATH}")
+    endforeach()
+    
+    # If this folder is not the library-build directory, push the updated item list to the parent scope
+    if(NOT LIB_BUILD_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
+        set(${ABS_TYPE} ${${ABS_TYPE}} PARENT_SCOPE)
+    endif()
+
+endmacro()
